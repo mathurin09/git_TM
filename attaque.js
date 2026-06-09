5ae2217950c2dedc9b82180a814c9718131b28d0
Mathurin Velas <mathurin.velas@edu.gbjb.ch>
2026-06-09 23:58:46 +0200

    [commencement du tableau excel et avancement de l'écrit]

--- attaque.js
+++ attaque.js
@@ -1,8 +1,8 @@
 import http from 'k6/http'
 
 export const options = {
-    vus : 1000, 
-    duration: '45s',
+    vus : 100, 
+    duration: '30s',
 }; 
 export default function() {
     http.get('http://servercrash.example.com')
